ba330364dc1bf30557c56b9ded795d51d61e1cbf
yehudi lam <yikyehudi.heilam@edfenergy.com>
2024-03-12 10:12:00 +0000

    page2

---
+++ Page2.tsx
@@ -0,0 +1,13 @@
+import Page3 from 'Page3';
+import {View, Text} from 'react-native';
+
+const Page2 = () => {
+  return (
+    <View>
+      <Page3 />
+      <Text>Page 2 content</Text>
+    </View>
+  );
+};
+
+export default Page2;
